8a13fea95eab70802571ec89c7b127a0dc450d3c
Berke Altıparmak <balpa@Berke-MacBook-Air.local>
2022-04-28 16:42:25 +0300

    winner logic done, need to add draw sit. -MAC

--- src/components/TicTactoe.tsx
+++ src/components/TicTactoe.tsx
@@ -8,10 +8,9 @@ function TicTactoe() {
     const [board, setBoard] = useState(Array(9).fill(null))
     const [currentPlayer, setCurrentPlayer] = useState('X')
     const [lastPlayer, setLastPlayer] = useState(null)
+    const [winner, setWinner] = useState(null)
 
 
-
-    console.log(board)
 
     const Square: React.FC<{squareId: number}> = ({ squareId }) => { 
 
@@ -37,12 +36,50 @@ function TicTactoe() {
 
     useEffect(()=> {
         currentPlayer === 'X' ? setCurrentPlayer('O') : setCurrentPlayer('X')
+
+        // CHECK IF THE MACTH HAS FINISHED
+
+        const winningCombinations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+
+        // CHECKS FOR THE CURRENT PLAYER'S MOVES AND THE WINNING COMBINATIONS IF THEY MATCH
+        const checkWin = (player: string) => {
+            return winningCombinations.some(combination => {
+                return combination.every(index => board[index] === player)
+            })
+        }
+
+
+        // CHECK FOR THE WINNING COMBINATION POSITIONS, RETURNS AN ARRAY
+        const whichCombination = () => {
+            for (let i = 0; i < winningCombinations.length; i++) {
+                const combination = winningCombinations[i]
+                if (board[combination[0]] === currentPlayer && board[combination[1]] === currentPlayer && board[combination[2]] === currentPlayer) {
+                    return combination
+                }}}
+
+
+        if (checkWin(currentPlayer) === true){
+            setWinner(currentPlayer)
+
+
+        }
+
     },[lastPlayer])
 
 
   return (
     <>
     <div className='current-player'>Current player is: {currentPlayer}</div>
+    <div className='winner'>{winner != null ? `Winner is: ${winner}` : ""}</div>
     <div className='frame'>
         <Square squareId={0}/>
         <Square squareId={1}/>
